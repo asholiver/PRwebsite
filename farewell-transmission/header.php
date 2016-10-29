80f3e5ed6a2fe6f1cc77a82547b18b42d1039a4f
Ashley Oliver <ashnotadvised@gmail.com>
2016-10-29 13:59:07 +0100

    font change

--- farewell-transmission/header.php
+++ farewell-transmission/header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:700" rel="stylesheet">
 
         <!-- favicons -->
         <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon.png">
